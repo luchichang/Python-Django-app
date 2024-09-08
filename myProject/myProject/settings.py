3a2a0238273925f613c755edf766a54f88e8f2b3
Dinesh <73271388+luchichang@users.noreply.github.com>
2024-09-08 23:13:40 +0530

    Bug Fix!

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
